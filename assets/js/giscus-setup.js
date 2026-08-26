263ad8e73ac04e257e123bd931c7392a4a0078bd
ktwall12 <123406984+ktwall12@users.noreply.github.com>
2026-08-26 19:22:47 +0000

    Deploying to gh-pages from @ ktwall12/ktwall12.github.io@11cd8ecadaeb92603dece4d9d90c9e6ad5869631 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "wer2ct/wer2ct.github.io",
+    "data-repo": "ktwall12/ktwall12.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
